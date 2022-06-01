47b9ed79876c32f03ad6eef86cc832335dfcbb91
werstudent-pc <werkstudent@dolle.com>
2022-06-01 13:08:17 +0200

    connecting to firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { userAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBx6VUExSpLk-T6hGqFKm_idPTKXZt2oic",
+    authDomain: "chat1-8bd04.firebaseapp.com",
+    projectId: "chat1-8bd04",
+    storageBucket: "chat1-8bd04.appspot.com",
+    messagingSenderId: "487838768485",
+    appId: "1:487838768485:web:240eb768fc07c6baaf958c",
+    measurementId: "G-E8Q4MN4GGK"
+
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+       
+    
     </div>
   );
 }
